41dbb15a98d1e8575a364229ea14800d8c455a1f
facumasino <facujmasino@gmail.com>
2023-08-30 21:38:28 -0300

    add basic structure

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << R"(
+  _   __    __        __    __ 
+ | | / /__ / /____ __/ /___/ /_
+ | |/ / -_) __/ -_)_  __/_  __/
+ |___/\__/\__/\__/ /_/   /_/   
+                               
+  )";
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
