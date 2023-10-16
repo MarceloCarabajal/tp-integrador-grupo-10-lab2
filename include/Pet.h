210cebd5b8acc25bf2878aa7f7c69782e8e38dbe
Ana Clara Bertello <bertelloana@outlook.com>
2023-10-16 01:38:42 -0300

    Agregue H de Clase Pet

--- include/Pet.h
+++ include/Pet.h
@@ -1,1 +1,36 @@
-PEPE
+#pragma once
+#include<string>
+
+class Pet {
+
+private: 
+int _petId, _ownerId;
+std::string _name, _specie,_breed, _currentDiagnosis;
+Date _birthDate;
+
+public: 
+ Pet();
+        void setPetId(int petId);
+        void setOwnerId (int ownerId)
+        void setName(std::string name);
+        void setSpecie(std::string specie);
+        void setBreed(std::string breed);
+        void setCurrentDiagnosis(std::string currentDiagnosis);
+        void setBirthDate (Date birthdate);
+     
+
+        int getPetId ();
+        int getOwnerId ();
+        std::string getName();
+        std::string getSpecie();
+        std::string getBreed();
+        std::string getCurrentDiagnosis();
+        Date getBirthDate ();
+
+        void addAppoinment ();
+        void loadPet ();
+        void recordVaccination ();
+
+};
+
+
