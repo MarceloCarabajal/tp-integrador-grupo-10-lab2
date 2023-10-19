77239747670b753ecfb9c4af55242e4254807efc
facumasino <facujmasino@gmail.com>
2023-10-19 19:43:18 -0300

    Se creo la clase Menu

--- include/menu.h
+++ include/menu.h
@@ -1,15 +1,18 @@
 #ifndef MENU_INCLUDED
 #define MENU_INCLUDED
 
-#include <iostream>
-
-#include "rlutil.h"
-
 const int DEFAULT_BG = rlutil::BLACK;
 
-void handleMenu();
-void printMenu(int selected);
-void printHighlighted(std::string text, bool highlight, int color = 14,
-                      int x = 0, int y = 0);
+class Menu {
+private:
+    int totalOptions, currentOption;
+    std::string *options;
+
+public:
+    void addOption(std::string option, int optNumber) int askOption();
+    void printMenu(int selected);
+    void printHighlighted(std::string text, bool highlight, int color = 14,
+                          int x = 0, int y = 0);
+};
 
 #endif /* MENU_INCLUDED */
