b2bba130ee8ae49fd3b9a6c2fae459f5b8ff22a2
MarceCarabajal <marcelo.carabajal@hasar.com>
2023-10-19 23:23:41 -0300

    creacion de .h de Date, Product y Time

---
+++ include/Product.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include<string>
+#include "ProductCategory.h"
+
+class Product{
+private:
+    int _productID, _quantity, _stock;
+    float _price;
+    std::string _productName, _description, _brand, _productType;
+    ProductCategory _productCategory
+public:
+    void setProductId(int productId);
+    void setProductName(std::string productName);
+    void setDescription(std::string description);
+    void setQuantity(int quantity);
+    void setStock(int stock);
+    void setPrice(float price);
+    void setBrand(std::string brand);
+    void setProductType(std::string productType);
+    void setProductCategory(ProductCategory category);
+
+    int getProductId();
+    std::string getName();
+    int getQuantity();
+    int getStock();
+    float getPrice();
+    std::string getBrand();
+    std::string getProductType();
+    std::string getProductCategory();
+
+    void addProduct();
+    void deleteProduct();
+    void updateStock();
+};
