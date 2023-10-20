0e7f35df2d60eb4931aeda4b582a366f5addd87d
Ana Clara Bertello <bertelloana@outlook.com>
2023-10-20 02:24:25 -0300

    Se Agrega Transaction.h

---
+++ include/Transaction.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <string>
+
+class Transaction {
+
+private:
+
+int _idTransaction;
+Date _date;
+Time _time;
+std::string _description;
+float _amount;
+
+public:  
+
+void setIdTransaction(int idTrx );
+void setDate( Date date);
+void setTime ( Time time);
+void setDescription(std:: string desc );
+void setAmount(float amount );
+
+int getIdTransaction();
+Date getDate();
+Time getTime();
+std::string +getDescription();
+float getAmount();
+
+
+};
