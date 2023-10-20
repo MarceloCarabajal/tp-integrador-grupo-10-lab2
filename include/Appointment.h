54289fca0eff57ca56b7628e2255ed2fcd2e9729
Ana Clara Bertello <bertelloana@outlook.com>
2023-10-19 23:51:18 -0300

    Se agrega Appointment.h

---
+++ include/Appointment.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+
+
+class Appointment {
+
+private: 
+ int _appId, _urgencyCat, _petId;
+ Date _date;
+ Time _time;
+ 
+ std:: string _reason; 
+ bool _attended;
+ int _clientId;
+ 
+
+ public: 
+ void setAppId( int id);
+ void setDate( Date date);
+ void setTime( Time time);
+ void setUrgencyCat( int cat);
+ void setReason( std::string reason);
+ void setAttended( bool attended);
+
+ int getAppId();
+ Date getDate();
+ Time getTime();
+ int getUrgencyCat();
+ std::string getReason();
+ bool getAttended();
+ 
+void load();
+};
