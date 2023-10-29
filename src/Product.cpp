b2faae88dbe8e897bc0b4e7a13574ea9c487c992
Ana Clara Bertello <bertelloana@outlook.com>
2023-10-29 00:23:05 -0300

    Se agregan metodos en product

--- src/Product.cpp
+++ src/Product.cpp
@@ -1,58 +1,27 @@
 #include "Product.h"
 
-void setProductId(int productId){
-    
-}
-void setProductName(std::string productName){
-    
-}
-void setDescription(std::string description){
-    
-}
-void setQuantity(int quantity){
-    
-}
-void setStock(int stock){
-    
-}
-void setPrice(float price){
-    
-}
-void setBrand(std::string brand){
-    
-}
-void setProductType(std::string productType){
-    
-}
-void setProductCategory(ProductCategory category){
-    
-}
 
-int getProductId(){
-    
-}
-std::string getProductName(){
-    
-}
-int getQuantity(){
-    
-}
-int getStock(){
-    
-}
-float getPrice(){
-    
-}
-std::string getBrand(){
-    
-}
-std::string getProductType(){
-    
-}
-ProductCategory getProductCategory(){
-    
-}
+void Product::setProductId(int productId){_productID=productId;   }
+void Product::setProductName(std::string productName){_productName=productName;}
+void Product::setDescription(std::string description){_description=description;}
+void Product::setQuantity(int quantity){_quantity=quantity;}
+void Product::setStock(int stock){_stock=stock;}
+void Product::setPrice(float price){_price=price;}
+void Product::setBrand(std::string brand){_brand=brand;}
+void Product::setProductType(std::string productType){_productType=productType;}
+void Product::setProductCategory(ProductCategory category){_productCategory=category;}
 
+int Product::getProductId(){return _productID;}
+std::string Product::getProductName(){return _productName;  }
+ std::string Product::getDescription (){return _description;}
+int Product::getQuantity(){return  _quantity;}
+int Product::getStock(){return _stock;}
+float Product::getPrice(){return _price;}
+std::string Product::getBrand(){return _brand; }
+std::string Product::getProductType(){return _productType ;}
+ProductCategory Product::getProductCategory(){return _productCategory;}
+
+//TODO Completar metodos restantes
 void addProduct(){
     
 }
