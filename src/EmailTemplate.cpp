6eec928e64c4d8fcf273617d09b311a7a0299207
facumasino <facujmasino@gmail.com>
2023-11-02 20:02:30 -0300

    Se documenta EmailTemplate

--- src/EmailTemplate.cpp
+++ src/EmailTemplate.cpp
@@ -1,10 +1,31 @@
 #include "EmailTemplate.h"
 
+/**
+ * @brief Constructor de la clase EmailTemplate.
+ *
+ * Este constructor crea una instancia de la clase EmailTemplate y especifica el
+ * archivo de plantilla de correo electrónico.
+ *
+ * @param templateFile El nombre del archivo de plantilla de correo electrónico.
+ */
 EmailTemplate::EmailTemplate(std::string templateFile) {
     _templateFile = templateFile;
     getHTMlTemplate();
 }
 
+/**
+ * @brief Establece los datos del correo electrónico.
+ *
+ * Esta función permite establecer los datos del correo electrónico, como el
+ * nombre del destinatario, el título del cuerpo del mensaje y la fecha de
+ * vencimiento.
+ *
+ * @param recipientName El nombre del destinatario.
+ * @param bodyTitle El título del cuerpo del mensaje.
+ * @param expDate La fecha de vencimiento del mensaje.
+ * @return `true` si se establecen los datos con éxito, de lo contrario,
+ * `false`.
+ */
 bool EmailTemplate::setEmailData(std::string recipientName,
                                  std::string bodyTitle, Date expDate) {
     bool success;
@@ -14,8 +35,22 @@ bool EmailTemplate::setEmailData(std::string recipientName,
     return success;
 }
 
+/**
+ * @brief Obtiene el contenido HTML del correo electrónico.
+ *
+ * Esta función devuelve el contenido HTML del correo electrónico después de
+ * haber establecido los datos del correo.
+ *
+ * @return El contenido HTML del correo electrónico.
+ */
 std::string EmailTemplate::getHTML() { return _HTMLData; }
 
+/**
+ * @brief Obtiene el contenido de la plantilla HTML.
+ *
+ * Esta función abre el archivo de plantilla HTML, lee su contenido y lo
+ * almacena internamente en la instancia de EmailTemplate.
+ */
 void EmailTemplate::getHTMlTemplate() {
     std::string strHTML;
     FILE *pHTML = fopen(_templateFile.c_str(), "rb");
@@ -33,6 +68,17 @@ void EmailTemplate::getHTMlTemplate() {
     if (sizeRead == fSize) _HTMLData = strHTML;
 }
 
+/**
+ * @brief Reemplaza un parámetro en el contenido HTML del correo electrónico.
+ *
+ * Esta función reemplaza un parámetro específico en el contenido HTML del
+ * correo electrónico con un nuevo valor.
+ *
+ * @param paramKey La clave del parámetro que se desea reemplazar.
+ * @param newValue El nuevo valor que se utilizará para reemplazar el parámetro.
+ * @return `true` si el reemplazo se realiza con éxito, de lo contrario,
+ * `false`.
+ */
 bool EmailTemplate::replaceParameter(std::string paramKey,
                                      std::string newValue) {
     int paramPos = 0;
