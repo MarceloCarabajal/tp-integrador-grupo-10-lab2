a54a19789957b55d8fa134a14b332a65f3f28158
Ana Clara Bertello <bertelloana@outlook.com>
2023-11-14 22:26:34 -0300

    Se corrige PetRelations h  y cpp

--- include/PetRelations.h
+++ include/PetRelations.h
@@ -4,9 +4,11 @@
 class PetRelations {
 
 private:
+int _relationId;
 int _petId;
 int  _clientId;
 bool _owner;
+bool _estatus;
 
 
 public: 
@@ -14,10 +16,13 @@ public:
 void setPetId(int id);
 void setClientId( int id) ;
 void  setOwner( bool owner);
+void setRelationId (int rid);
+void setEstatus (bool est);
 
 int  getPetId();
 int  getClientId();
 bool  getOwner();
-
+int getRelationId ();
+bool getEstatus ();
 
 };
--- src/PetRelations.cpp
+++ src/PetRelations.cpp
@@ -1,9 +1,14 @@
 #include "PetRelations.h"
 
-void PetRelations::setPetId(int id) {}
-void PetRelations::setClientId(int id) {}
-void PetRelations::setOwner(bool owner) {}
+void PetRelations::setPetId(int id) {_petId=id;}
+void PetRelations::setClientId(int id) {_clientId=id;}
+void PetRelations::setOwner(bool owner) {_owner=owner;}
+void PetRelations::setRelationId (int rid){_relationId=rid;}
+void PetRelations::setEstatus (bool est){_estatus=est;}
 
-int PetRelations::getPetId() {}
-int PetRelations::getClientId() {}
-bool PetRelations::getOwner() {}
+int PetRelations::getPetId() {return _petId; }
+int PetRelations::getClientId() {return _clientId; }
+bool PetRelations::getOwner() {return _owner;}
+int PetRelations::getRelationId (){return _relationId;}
+bool PetRelations::getEstatus(){return _estatus;}
+
