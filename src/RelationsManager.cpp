87ba12ab9b9ff85ee972b4d8c153dae5f7024215
Ana Clara Bertello <bertelloana@outlook.com>
2023-11-22 15:10:32 -0300

    Correcc Relat

--- src/RelationsManager.cpp
+++ src/RelationsManager.cpp
@@ -151,7 +151,7 @@ void RelationsManager::edit() {
     // Si se encontro, pedir datos
     PetRelations auxPetR = editForm(regPos);
     // Si no se completo el formulario, salir
-    if (auxPetR.getRelationId() == -1) {
+    if (auxPetR.getPetId() == -1) {
         std::cout << "No se realizara la edicion.\n";
         utils::pause();
         return;
