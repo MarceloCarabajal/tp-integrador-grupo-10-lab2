8ac79cd398c2761f32edf661cb93b3ce6096622e
facumasino <facujmasino@gmail.com>
2023-11-30 16:43:06 +0000

    Documenta MainManager

--- include/MainManager.h
+++ include/MainManager.h
@@ -4,15 +4,33 @@
 #include "ClientsManager.h"
 #include "Menu.h"
 #include "SubMenus.h"
+
+/**
+ * @brief Clase que representa el administrador principal del programa.
+ *
+ * Esta clase se encarga de gestionar el menú principal y los submenús del
+ * programa. También verifica la configuración antes de iniciar el programa.
+ */
 class MainManager {
 private:
     Menu _mainMenu = Menu("MENU PRINCIPAL:house:");
     SubMenus _subMenus;
 
+    /**
+     * @brief Verifica la configuración del programa.
+     * @return true si la configuración es válida, false en caso contrario.
+     */
     bool checkConfig();
 
 public:
+    /**
+     * @brief Inicia el programa.
+     */
     void start();
+
+    /**
+     * @brief Muestra el menú principal.
+     */
     void mainMenu();
 };
 
