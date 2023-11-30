b6e23559ecea4138d5bed7630b60765d29e24453
facumasino <facujmasino@gmail.com>
2023-11-30 11:20:06 +0000

    Corrige conflictos en AppointmentsManager.cpp

--- src/AppointmentsManager.cpp
+++ src/AppointmentsManager.cpp
@@ -122,7 +122,6 @@ Appointment AppointmentsManager::editForm(int regPos) {
     InputForm appForm(true), petIdForm(true, true), clientIdForm(true),
         dateForm(true), timeForm(true);
     Appointment auxApp, auxFormApp;
-    RelationsManager relationsMgr;
     PetsManager petsManager;
     ClientsManager clientsManager;
     Date appDate;
@@ -163,17 +162,6 @@ Appointment AppointmentsManager::editForm(int regPos) {
         if (!clientIdForm.fill()) return auxFormApp;
         existentId = clientsManager.idExists(clientId);
         if (!retryIfIdNotExists(existentId)) return auxFormApp;
-    }
-
-    // Validar que exista una relacion activa cliente/mascota
-    bool relationExists = relationsMgr.relationExists(petId, clientId);
-    if (!relationExists) {
-        std::cout << "No existe una relación activa entre el cliente y la "
-                     "mascota ingresados.\n";
-        std::cout
-            << "Por favor cargue la relación desde el menú 'Relaciones'.\n";
-        utils::pause();
-        return auxApp;
     }
 
     // pedir y validar fecha
@@ -227,9 +215,6 @@ void AppointmentsManager::edit() {
     InputForm search;
     int nId;
     show(false);
-    int totalRegs= _appsFile.getTotalRegisters();
-    if (totalRegs<=0) return;
-
     std::cout << "\nIngrese el ID del turno a modificar.\n";
     search.setIntField("ID Turno", nId, 4);
     if (!search.fill()) return;  // si no se completa, salir
@@ -268,12 +253,6 @@ void AppointmentsManager::show(bool showAndPause) {
         utils::pause();
         return;
     }
-
-    if (totalRegs == 0) {
-        std::cout << "No hay registros para mostrar.\n";
-        utils::pause();
-        return;}
-        
     // Se crea la variable que va a contener todas las celdas, segun la cantidad
     // de registros
     std::string* cells = new std::string[totalCells];
@@ -421,6 +400,36 @@ void AppointmentsManager::cancel() {
         std::cout << "Ocurrió un error al intentar cancelar el turno.\n";
     }
     utils::pause();
+}
+
+int AppointmentsManager::getActiveAppsCount() {
+    int total = _appsFile.getTotalRegisters();
+    if (total < 0) return -1;
+    int active = 0;
+    for (int i = 0; i < total; i++) {
+        Appointment auxApp = _appsFile.readFile(i);
+        if (auxApp.getStatus()) active++;
+    }
+    return active;
+}
+
+Appointment* AppointmentsManager::getActiveApps() {
+    int total = _appsFile.getTotalRegisters();
+    if (total <= 0) return NULL;
+    int totalActive = getActiveAppsCount();
+    int activeCount = 0;
+    Appointment* apps = new Appointment[total];
+    Appointment* activeApps = new Appointment[totalActive];
+    if (apps == NULL) return NULL;
+    if (activeApps == NULL) return NULL;
+    if (!_appsFile.readFile(apps, total)) return NULL;
+    for (int i = 0; i < total; i++) {
+        if (apps[i].getStatus()) {
+            activeApps[activeCount] = apps[i];
+            activeCount++;
+        }
+    }
+    return activeApps;
 }
 
 int AppointmentsManager::getPendingApps() {
